475b2ac068db3488d6eb2ecf7affc4457534938b
Mike Smart <mike.smart@digitalframework.com>
2019-01-31 20:56:55 +0000

    Created solution and client project

---
+++ src/HubitatInfluxLogger/Client/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HubitatInfluxLogger.Client
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            while (true)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(60));
+            }
+        }
+    }
+}
